96c9b436345d24b53be34e6d2cd42e54d82c5aef
TeeTeeEm <129229896+TeeTeeEm@users.noreply.github.com>
2023-04-27 03:45:09 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from tkinter import *
-from tkinter import messagebox
 from random import choice, randint, shuffle
-import pyperclip
+from tkinter import messagebox
+
+import pyperclip, json
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 # Password Generator Project
 def generator():
     password_enter.delete(0, END)
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -23,8 +28,21 @@ def generator():
     pyperclip.copy(new_password)
 
 
-
-
+# ---------------------------- PASSWORD SEARCHER ------------------------------- #
+def find_password():
+    website = website_enter.get()
+    try:
+        with open("Passwords.json") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Oops", message=f"Nothing found.")
+    else:
+        if website in data:
+            login = data[website]["login"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {login}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Oops", message=f"Nothing found related to {website}.")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -32,16 +50,27 @@ def add_in_txt():
     website = website_enter.get()
     login = login_enter.get()
     password = password_enter.get()
+    new_data = {
+        website: {
+            "login": login,
+            "password": password
+        }
+    }
     if website == "" or login == "" or password == "":
         messagebox.showinfo(title="Ooops", message="Please don't leave any fields empty")
     else:
-        yes = messagebox.askyesno(title=website, message=f"Your login is {login} and passwords is {password}")
-        if yes:
-            with open("Passwords.txt", "a") as file:
-                file.write(
-                    f"{website} | {login} | {password}\n")
+        try:
+            with open("Passwords.json", "r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            with open("Passwords.json", "w") as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            data.update(new_data)
+            with open("Passwords.json", "w") as file:
+                json.dump(data, file, indent=4)
+        finally:
             website_enter.delete(0, END)
-            login_enter.delete(0, END)
             password_enter.delete(0, END)
 
 
@@ -62,8 +91,8 @@ login.grid(row=2, column=0)
 password = Label(text="Password: ")
 password.grid(row=3, column=0)
 
-website_enter = Entry(width=39)
-website_enter.grid(row=1, column=1, columnspan=2)
+website_enter = Entry(width=21)
+website_enter.grid(row=1, column=1)
 website_enter.focus()
 login_enter = Entry(width=39)
 login_enter.grid(row=2, column=1, columnspan=2)
@@ -71,10 +100,13 @@ login_enter.focus()
 password_enter = Entry(width=21)
 password_enter.grid(row=3, column=1)
 password_enter.focus()
+
 generate_button = Button(text="Generate Password", command=generator)
 generate_button.grid(row=3, column=2)
 save_button = Button(text="Add", width=34, command=add_in_txt)
 save_button.grid(row=4, column=1, columnspan=2)
+search_button = Button(text="Search", command=find_password, width=14)
+search_button.grid(row=1, column=2)
 
 # def generate():
 
